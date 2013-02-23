7786a3e6e7f165d7cb499c1d5f3378c5c6def85a
Dariusz Kuzma <netmik12@gmail.com>
2013-02-23 18:49:38 +0100

    Update combat tab

--- src/colloid/MainController.java
+++ src/colloid/MainController.java
@@ -5,10 +5,13 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -42,6 +45,8 @@ public class MainController extends AnchorPane implements Initializable {
     TitledPane recountLogPane;
     @FXML
     MenuItem menuItemClose;
+    @FXML
+    ListView<String> combatListView;
 
     private App application;
 
@@ -57,6 +62,10 @@ public class MainController extends AnchorPane implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         textLog.setEditable(false);
         recountLog.setEditable(false);
+
+        ObservableList<String> items = FXCollections.observableArrayList();
+        combatListView.setItems(items);
+//        combatListView.itemsProperty().bind();
     }
 
     public App getApplication() {
